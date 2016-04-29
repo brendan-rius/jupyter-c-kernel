ae6911c3be172d2d163ddbfb27e90e97b7c61684
Brendan Rius <brendan@omixy.com>
2016-04-29 10:01:38 +0100

    Change version from 1.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='jupyter_c_kernel',
-      version='1.0',
+      version='1.0.0',
       description='Minimalistic C kernel for Jupyter',
       author='Brendan Rius',
       author_email='ping@brendan-rius.com',
