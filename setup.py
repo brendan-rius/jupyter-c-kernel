b2f114277ed1c3f869be533aaf950d4488c5e43d
Brendan Rius <brendan.rius@gmail.com>
2017-08-13 17:01:59 +0200

    Bump to 1.2.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='jupyter_c_kernel',
-      version='1.2.0',
+      version='1.2.1',
       description='Minimalistic C kernel for Jupyter',
       author='Brendan Rius',
       author_email='ping@brendan-rius.com',
       url='https://github.com/brendanrius/jupyter-c-kernel/',
-      download_url='https://github.com/brendanrius/jupyter-c-kernel/tarball/1.2.0',
+      download_url='https://github.com/brendanrius/jupyter-c-kernel/tarball/1.2.1',
       packages=['jupyter_c_kernel'],
       scripts=['jupyter_c_kernel/install_c_kernel'],
       keywords=['jupyter', 'notebook', 'kernel', 'c']
